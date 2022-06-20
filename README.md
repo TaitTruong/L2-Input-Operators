848e46a39dd522b9eecac6e41b0ea1567c6fa5cc
Tait <107879979+TaitTruong@users.noreply.github.com>
2022-06-20 19:03:26 +0000

    story

---
+++ README.md
@@ -0,0 +1,17 @@
+## Input
+Input is the wau the computer gets information from the user
+
+Note: Pythpn innitially input as a `string` type
+
+Example;
+
+5 --> "5"
+
+Today is Monday ---> "Today is Monday"
+
+### Formula
+`variableName = input("PROMPT")`
+
+PROMPT - A question or statement that the computer asks the user
+
+Python stores the __input__ in the __variableName__}
---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+userName = input("Enter your name")
+("Hey " + userName)
+
+userGrade = input("What grade are you in? ")
+print("Hey " + userName+ " " + userGrade + " is a lame grade")
+
+userYear = int(input("What year were you born? "))
+userAge = 2022 - userYear
+print(str(userAge) + " is so young!")
+
+userIp = input("Please enter your credit card number and you will win a lot of money:")
+print("Thank you!")
+'''
+
+#Create your own story Madlibs
+#Have at least 5 inputs
+
+
+ = input("")
